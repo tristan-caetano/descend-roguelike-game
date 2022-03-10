eb67ce43b30eff7ff64828a67c673d4b5aa8d827
tristan-caetano <tristancostacaetano@gmail.com>
2022-03-09 20:08:33 -0500

    Fixed attack and added spell animations

--- Assets/Mouse_Pointer.cs
+++ Assets/Mouse_Pointer.cs
@@ -9,12 +9,20 @@ public class Mouse_Pointer : MonoBehaviour
     public GameObject crosshairs;
     public GameObject player;
     public GameObject magicPrefab;
-
+    public Animator animator;
+    public Transform attackPoint;
+    public float attackRange = .5f;
     public float spellSpeed = 1f;
+    float lastH, lastV;
+    public LayerMask enemyLayers;
+    public Vector3 mouseInput;
 
     // Start is called before the first frame update
     void Start(){
         Cursor.visible = false;
+        lastH = 0;
+        lastV = 0;
+    
     }
 
     // Update is called once per frame
@@ -27,21 +35,59 @@ public class Mouse_Pointer : MonoBehaviour
         // Weird rotation thing
         Vector3 difference = target - player.transform.position;
         float rotationZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
-        // player.transform.rotation = Quaternion.Euler(0.0f, 0.0f, rotationZ);
 
         if(Input.GetMouseButtonDown(0)){
+            animator.SetFloat("LastH", difference.x);
+            animator.SetFloat("LastV", difference.y);
+            animator.SetTrigger("Cast");
             float distance = difference.magnitude;
             Vector2 direction = difference / distance;
             direction.Normalize();
             fireBullet(direction, rotationZ);
         }
-        
+
     }
 
+    void FixedUpdate(){
+
+        target = transform.GetComponent<Camera>().ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z));
+        crosshairs.transform.position = new Vector2(target.x, target.y);
+
+        // Weird rotation thing
+        Vector3 difference = target - player.transform.position;
+        float rotationZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+
+        if(Input.GetKey(KeyCode.Space)){
+            animator.SetFloat("LastH", difference.x);
+            animator.SetFloat("LastV", difference.y);
+            Attack();
+        }
+    }
+
+// Method that calculated and fires spell
     void fireBullet(Vector2 direction, float rotationZ){
         GameObject b = Instantiate(magicPrefab) as GameObject;
         b.transform.position = player.transform.position;
         b.transform.rotation = Quaternion.Euler(0.0f, 0.0f, rotationZ);
         b.GetComponent<Rigidbody2D>().velocity = direction * spellSpeed;
+    }
+
+// Method that calculates melee attack
+    void Attack(){
+        animator.SetTrigger("Attack");
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+
+        foreach(Collider2D enemy in hitEnemies){
+            Debug.Log("Hit!");
+        }
+    }
+
+    void OnDrawGizmosSelected(){
+
+        if(attackPoint == null){
+            return;
+        }
+
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
     }
 }
--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -10,16 +10,10 @@ public class PlayerMovement : MonoBehaviour
     public Rigidbody2D rb;
     public Animator animator;
     Vector2 movement;
-    float lastH, lastV;
     public Transform attackPoint;
     public float attackRange = .5f;
     public LayerMask enemyLayers;
 
-    void Start()
-    {
-       lastH = 0;
-       lastV = 0;
-    }
     
 
     // Update is called once per frame
@@ -43,15 +37,6 @@ public class PlayerMovement : MonoBehaviour
         animator.SetFloat("Vertical", movement.y);
         animator.SetFloat("Speed", movement.sqrMagnitude);
 
-        if(movement.sqrMagnitude > 0.01){
-            lastH = movement.x;
-            lastV = movement.y;
-        }
-
-        
-
-        //animator.SetBool("isAttack", false);
-
     }
 
     void FixedUpdate()
@@ -59,32 +44,6 @@ public class PlayerMovement : MonoBehaviour
         
         // Player movement
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
-
-        if(Input.GetKey(KeyCode.Space)){
-            animator.SetFloat("LastH", lastH);
-            animator.SetFloat("LastV", lastV);
-            Attack();
-        }
-
-        animator.SetBool("isAttack", false);
-
     }
 
-    void Attack(){
-        animator.SetTrigger("Attack");
-        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
-
-        foreach(Collider2D enemy in hitEnemies){
-            Debug.Log("Hit!");
-        }
-    }
-
-    void OnDrawGizmosSelected(){
-
-        if(attackPoint == null){
-            return;
-        }
-
-        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
-    }
 }
